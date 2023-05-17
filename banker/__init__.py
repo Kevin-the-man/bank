c97e2bb6c7649b17b560eb89a9060f022232c70d
Kevin-the-man <56621344+Kevin-the-man@users.noreply.github.com>
2023-05-17 15:55:38 -0700

    Update __init__.py

--- banker/__init__.py
+++ banker/__init__.py
@@ -10,13 +10,11 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
         app.config.from_pyfile("config.py", silent=True)
     else:
-        # load the test config if passed in
         app.config.update(test_config)
 
-    # ensure the instance folder exists
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
@@ -31,12 +29,10 @@ def create_app(test_config=None):
     @app.errorhandler(404)
     @app.errorhandler(410)
     def page_not_found(e):
-        # note that we set the 404 status explicitly
-        return "ERRO, please check your network and login again"
+        return "ERROR, please login again"
 
     from banker import db
     db.init_app(app)
-    # apply the blueprints to the app
     from banker import auth,bank
     app.register_blueprint(auth.bp)
     app.register_blueprint(bank.bp)
